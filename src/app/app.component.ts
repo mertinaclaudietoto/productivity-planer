4245a419b0ec67ae3af521b8fc480a3a00a95400
AlinaMertina <mertinaclaudietoto@gmail.com>
2025-03-23 13:31:30 +0300

    chore[webapp]:initalisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'producivity-planer';
+  
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readonly isstaging= !(environment as any).production
 }
